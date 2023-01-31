fc33560f3cfc72fca957260e3577c235897baef5
kiipuri <kiipuri01@gmail.com>
2023-01-31 04:14:23 +0200

    improve user search box

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,25 +1,23 @@
 <template>
-    <div id="search">
-        <v-text-field
-            hide-details
-            prepend-icon="mdi-magnify"
-            single-line
-            label="Search users"
-            @input="searchUsers"
-            v-model="searchText">
-        </v-text-field>
-        <v-list v-if="foundUsers.length > 0">
-            <template v-for="user in foundUsers">
-                <router-link :to="'/users/' + user._id" style="text-decoration: none">
-                    {{ user.username }}
-                </router-link>
-            </template>
-        </v-list>
+  <div id="search">
+    <v-text-field hide-details single-line label="Search users" @input="searchUsers" v-model="searchText">
+    </v-text-field>
+    <v-card class="mx-auto" max-width="300" v-if="foundUsers.length > 0">
+      <v-list>
+        <v-list-item
+          v-for="(user, i) in foundUsers"
+          :key="i"
+          :value="user"
+          router :to="'/users/' + user._id">
+          {{ user.username }}
+        </v-list-item>
+      </v-list>
+    </v-card>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from "axios"
 
 export default {
   data() {
@@ -28,28 +26,31 @@ export default {
       foundUsers: [],
       hidden: true,
       clicked: false,
-    };
+    }
   },
   methods: {
     searchUsers() {
-      this.foundUsers = [];
+      this.foundUsers = []
       axios
         .post(`http://localhost:9000/api/find-users/${this.searchText}`)
         .then((res) => {
-          this.foundUsers = res.data;
+          this.foundUsers = res.data
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     },
   },
   watch: {
     $route() {
-      this.searchText = "";
-      this.foundUsers = [];
+      this.searchText = ""
+      this.foundUsers = []
     },
   },
-};
+  computed: {
+    
+  },
+}
 </script>
 
 <style scoped>
@@ -57,10 +58,12 @@ export default {
   margin: 1em auto;
   width: 50%;
 }
+
 #float {
   position: absolute;
   width: 20%;
 }
+
 .dropdown-item {
   background-color: #f5f1f1;
 }
