970562352406b36631ca33afc36bac35de7162e2
kiipuri <kiipuri01@gmail.com>
2023-02-05 07:57:16 +0200

    use prettier as formatter

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueJsx from "@vitejs/plugin-vue-jsx"
 
-import path from "path";
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  root: path.resolve(__dirname, "./client"),
+  root: path.resolve(__dirname, "./"),
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./client/src', import.meta.url))
+      "@": fileURLToPath(new URL("./client/src", import.meta.url)),
     }
   }
 })
