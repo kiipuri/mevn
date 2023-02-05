840fa97169759716b138827b2c4de3381a0ca08e
kiipuri <kiipuri01@gmail.com>
2023-02-05 07:52:22 +0200

    make clicking on a post link to the posts page

--- client/src/components/UserPost.vue
+++ client/src/components/UserPost.vue
@@ -1,72 +1,68 @@
 <template>
-  <v-card
-    v-if="isDataFetched"
-    class="my-3"
-  >
-    <v-card-title class="text-subtitle-1 mt-1">
-      <v-avatar class="mr-2">
-        <v-img
-          :src="'http://localhost:9000/api/get-image/' + user.image"
-          alt="Avatar"
-        />
-      </v-avatar>
-      <router-link
-        :to="'/users/' + post.userID"
-        class="user-link"
+  <v-card v-if="isDataFetched" class="my-3">
+    <router-link
+      :to="'/post/' + post._id"
+      style="width: 100%; height: 100%; position: absolute"
+    />
+    <div class="px-4">
+      <v-card-title class="text-subtitle-1 mt-1 px-0">
+        <v-avatar class="mr-2">
+          <v-img
+            :src="'http://localhost:9000/api/get-image/' + user.image"
+            alt="Avatar"
+          />
+        </v-avatar>
+        <router-link :to="'/users/' + post.userID" class="user-link prel">
+          {{ user.username }}
+        </router-link>
+        <span class="float-right font-weight-regular">
+          {{ formatDate(post.createdAt) }}
+        </span>
+      </v-card-title>
+      <v-card-text class="text-subtitle-1 post-text mb-2 pa-0 prel dinl">
+        {{ post.post }}
+      </v-card-text>
+      <v-card-subtitle
+        class="text-subtitle-2 font-weight-bold actions pa-0 pointer-none"
       >
-        {{ user.username }}
-      </router-link> <span class="float-right font-weight-regular">
-        {{ formatDate(post.createdAt) }}
-      </span>
-    </v-card-title>
-    <v-card-text class="text-subtitle-1 post-text mb-n2">
-      {{ post.post }}
-    </v-card-text>
-    <v-card-subtitle class="text-subtitle-2 font-weight-bold actions">
-      <v-btn
-        icon="system-uicons:speech-bubble"
-        variant="plain"
-        size="x-small"
-        style="margin-bottom: 2px;"
-      >
-        <Icon
+        <v-btn
           icon="system-uicons:speech-bubble"
-          width="24"
-          height="24"
-        />
-      </v-btn>
-      {{ post.comments.length }}
-      <v-btn
-        icon="mdi-cached"
-        class="ml-2"
-        variant="plain"
-        size="x-small"
-        style="margin-bottom: 2px;"
-      >
-        <Icon
-          icon="entypo:retweet"
-          width="24"
-          height="24"
-        />
-      </v-btn>
-      250
-      <v-btn
-        icon="mdi-thumb-up"
-        class="ml-2"
-        variant="plain"
-        size="x-small"
-        style="margin-bottom: 2px;"
-        @click="likePost()"
-      >
-        <Icon
-          :icon="likeIcon"
-          width="24"
-          height="24"
-          :class="{ liked: liked }"
-        />
-      </v-btn>
-      {{ likes.length }}
-    </v-card-subtitle>
+          variant="plain"
+          class="pointer-all"
+          size="x-small"
+          style="margin-bottom: 2px"
+        >
+          <Icon icon="system-uicons:speech-bubble" width="24" height="24" />
+        </v-btn>
+        {{ post.comments.length }}
+        <v-btn
+          icon="mdi-cached"
+          class="ml-2 pointer-all"
+          variant="plain"
+          size="x-small"
+          style="margin-bottom: 2px"
+        >
+          <Icon icon="entypo:retweet" width="24" height="24" />
+        </v-btn>
+        250
+        <v-btn
+          icon="mdi-thumb-up"
+          class="ml-2 pointer-all"
+          variant="plain"
+          size="x-small"
+          style="margin-bottom: 2px"
+          @click="likePost()"
+        >
+          <Icon
+            :icon="likeIcon"
+            width="24"
+            height="24"
+            :class="{ liked: liked }"
+          />
+        </v-btn>
+        {{ likes.length }}
+      </v-card-subtitle>
+    </div>
   </v-card>
 </template>
 
@@ -79,10 +75,10 @@ export default {
   props: {
     post: {
       type: Object,
-      default: new Object()
+      default: () => ({}),
     },
   },
-  data () {
+  data() {
     return {
       user: Object,
       liked: false,
@@ -102,31 +98,37 @@ export default {
   methods: {
     formatDate,
     getUser() {
-      axios.get(`http://localhost:9000/api/get-user/${this.post.userID}`).then((res) => {
-        this.user = res.data
-      })
+      axios
+        .get(`http://localhost:9000/api/get-user/${this.post.userID}`)
+        .then((res) => {
+          this.user = res.data
+        })
         .catch((err) => {
           console.log(err)
         })
     },
     likePost() {
-      axios.post("http://localhost:9000/api/like-post", {
-        userID: this.$storage.getStorageSync("userid"),
-        postID: this.post._id
-      }).then((res) => {
-        this.liked = res.data.liked
-      })
+      axios
+        .post("http://localhost:9000/api/like-post", {
+          userID: this.$storage.getStorageSync("userid"),
+          postID: this.post._id,
+        })
+        .then((res) => {
+          this.liked = res.data.liked
+        })
     },
     getLikes() {
-      axios.get(`http://localhost:9000/api/get-post-likes/${this.post._id}`).then((res) => {
-        this.likes = res.data.map(el => el.userID)
-        if(this.likes.includes(this.$storage.getStorageSync("userid"))) {
-          this.liked = true
-        }
+      axios
+        .get(`http://localhost:9000/api/get-post-likes/${this.post._id}`)
+        .then((res) => {
+          this.likes = res.data.map((el) => el.userID)
+          if (this.likes.includes(this.$storage.getStorageSync("userid"))) {
+            this.liked = true
+          }
 
-        this.isDataFetched = true
-      })
-    }
+          this.isDataFetched = true
+        })
+    },
   },
 }
 </script>
--- client/src/views/UserPostPage.vue
+++ client/src/views/UserPostPage.vue
@@ -1,32 +1,53 @@
 <template>
-  <v-card class="my-6">
-    <v-card-title
-      class="text-subtitle-1 font-weight-bold"
-    >
-      <router-link
-        :to="'/users/' + user._id"
-        class="user-link"
-      >
+  <v-card v-if="isDataFetched" class="my-6">
+    <v-card-title class="text-subtitle-1 font-weight-bold">
+      <v-avatar class="mr-2">
+        <v-img
+          :src="'http://localhost:9000/api/get-image/' + user.image"
+          alt="Avatar"
+        />
+      </v-avatar>
+      <router-link :to="'/users/' + user._id" class="user-link">
         {{ user.username }}
       </router-link>
     </v-card-title>
-    <v-card-text class="text-h5">
+    <v-card-text class="text-h5" style="line-height: 2rem">
       {{ post.post }}
     </v-card-text>
     <v-card-subtitle>{{ formatDateTime(post.createdAt) }}</v-card-subtitle>
-    <v-card-subtitle class="text-subtitle-2 font-weight-bold actions">
-      <Icon
-        icon="heroicons:chat-bubble-oval-left-20-solid"
-        style="vertical-align: -2px;"
-      />
-      {{ replies.length }}
+    <v-card-subtitle class="text-subtitle-2 font-weight-bold actions mt-1">
+      <v-btn
+        icon="system-uicons:speech-bubble"
+        variant="plain"
+        size="x-small"
+        style="margin-bottom: 2px"
+      >
+        <Icon icon="system-uicons:speech-bubble" width="24" height="24" />
+      </v-btn>
+      {{ post.comments.length }}
+      <v-btn
+        icon="mdi-cached"
+        class="ml-2"
+        variant="plain"
+        size="x-small"
+        style="margin-bottom: 2px"
+      >
+        <Icon icon="entypo:retweet" width="24" height="24" />
+      </v-btn>
+      250
+      <v-btn
+        icon="mdi-thumb-up"
+        class="ml-2"
+        variant="plain"
+        size="x-small"
+        style="margin-bottom: 2px"
+      >
+        <Icon icon="mdi:cards-heart-outline" width="24" height="24" />
+      </v-btn>
+      10
     </v-card-subtitle>
   </v-card>
-  <UserPost
-    v-for="reply in replies"
-    :key="reply._id"
-    :post="reply"
-  />
+  <UserPost v-for="reply in replies" :key="reply._id" :post="reply" />
 </template>
 
 <script>
@@ -43,12 +64,13 @@ export default {
       post: Object,
       user: Object,
       replies: [],
+      isDataFetched: false,
     }
   },
   watch: {
-    $route () {
+    $route() {
       this.getPost()
-    }
+    },
   },
   beforeMount() {
     this.getPost()
@@ -56,21 +78,36 @@ export default {
   methods: {
     formatDateTime,
     getPost() {
-      axios.get(`http://localhost:9000/api/get-post/${this.$route.params.id}`).then((res) => {
-        this.post = res.data
-        this.getUser()
-        this.getReplies()
-      })
+      axios
+        .get(`http://localhost:9000/api/get-post/${this.$route.params.id}`)
+        .then((res) => {
+          this.post = res.data
+          this.getUser()
+          this.getReplies()
+          this.isDataFetched = true
+        })
         .catch((err) => {
           console.log(err)
         })
     },
     getUser() {
-      axios.get(`http://localhost:9000/api/get-user/${this.post.userID}`).then((res) => {
-        this.user = res.data
-      })
+      axios
+        .get(`http://localhost:9000/api/get-user/${this.post.userID}`)
+        .then((res) => {
+          this.user = res.data
+        })
         .catch((err) => {
           console.log(err)
+        })
+    },
+    likePost() {
+      axios
+        .post("http://localhost:9000/api/like-post", {
+          userID: this.$storage.getStorageSync("userid"),
+          postID: this.post._id,
+        })
+        .then((res) => {
+          this.liked = res.data.liked
         })
     },
     getReplies() {
@@ -83,6 +120,6 @@ export default {
           console.log(err)
         })
     },
-  }
+  },
 }
 </script>
